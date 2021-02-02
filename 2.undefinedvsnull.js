7e08a9f7d2e1dc2404915a7df2983bb388369e0e
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-02-02 20:01:25 +0600

    sfsadf

---
+++ 2.undefinedvsnull.js
@@ -0,0 +1,37 @@
+//
+let pakhi;
+console.log(pakhi);
+
+//
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+const result = add(3,4);
+console.log(result);
+
+//
+function add1(num1, num2) {
+    console.log(num1, num2);
+    return num1 + num2;
+}
+const result1 = add1(3);
+console.log(result1);
+
+//
+function add2(num1, num2) {
+    return 
+}
+const result2 = add2(3);
+console.log(result2);
+
+//
+const premika = {name: 'asdf'};
+console.log(premika.gf);
+
+//
+let fun = undefined;
+console.log(fun);
+
+//
+let ages = [1,2,3];
+console.log(ages[10]);
---
+++ 3.doubleequalvstrippleequal.js
@@ -0,0 +1,22 @@
+//
+const first = 2;
+const second = 2;
+if (first == second) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+
+//
+const first1 = 2;
+const second1 = "2";
+if (first1 == second1) {
+    console.log("true");
+}
+if (first1 === second) {
+    console.log('false');
+}
+else {
+    console.log("false");
+}
